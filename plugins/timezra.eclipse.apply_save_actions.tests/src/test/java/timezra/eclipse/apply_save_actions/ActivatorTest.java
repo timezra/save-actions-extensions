0b063e927d0638d9840bc9fea61d341ccc96f731
Tim Myer <timezra@gmail.com>
2011-10-18 12:40:11 -0700

    Adding canary for Activator to be in synch with https://github.com/timezra/tycho_new_plugin_project

---
+++ plugins/timezra.eclipse.apply_save_actions.tests/src/test/java/timezra/eclipse/apply_save_actions/ActivatorTest.java
@@ -0,0 +1,13 @@
+package timezra.eclipse.apply_save_actions;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+public class ActivatorTest {
+
+	@Test
+	public void thePluginShouldBeActivated() {
+		assertNotNull(Activator.getDefault());
+	}
+}
